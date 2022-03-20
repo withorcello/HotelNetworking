fd54a4ec2e7dc069daeff2f4e42af71c57061af3
WithorCello <withorlucas2009@gmail.com>
2022-03-20 13:42:20 -0400

    Alterações

---
+++ hotelnext/styles/pages/entry.ts
@@ -0,0 +1,7 @@
+import tw from 'tailwind-styled-components'
+
+export const Navbar = tw.div`
+    background: red;
+    width: 100%;
+    h-8;
+`;
---
+++ hotelnext/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'background-entry': "url('./public/img/idex.jpg')",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
